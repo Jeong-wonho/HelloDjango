e7d222459d9b1dcb5b96812019b15e595f9a82d8
wonho <kallzzang@naver.com>
2020-11-09 14:58:47 +0900

    (템플릿 설정 및 테스트)

--- board/urls.py
+++ board/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('list/', views.list),
+    path('view/', views.view),
+    path('write/', views.write),
+    path('update/', views.update),
+    path('delete/', views.delete)
 ]
