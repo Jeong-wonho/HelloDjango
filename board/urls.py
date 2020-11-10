20ddb190e338efd09842612db08c66fc6d5c3616
wonho <kallzzang@naver.com>
2020-11-10 16:58:49 +0900

    django 뷰 적용

--- board/urls.py
+++ board/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'board'
+
 urlpatterns = [
     path('', views.index),
-    path('list/', views.list),
-    path('view/', views.view),
-    path('write/', views.write),
-    path('update/', views.update),
-    path('delete/', views.delete)
+    path('list/', views.list, name='list'),
+    path('view/<int:bid>/', views.view),
+    path('write/', views.write, name='write'),
+    path('update/<int:bid>/', views.update),
+    path('delete/<int:bid>/', views.delete)
 ]
--- board/views.py
+++ board/views.py
@@ -1,21 +1,48 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from board.forms import BoardForm
+from board.models import Board
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
 
-def list(request):
-    return render(request, 'board/list.html')
+# def list(request):
+#     return render(request, 'board/list.html')
 
-def view(request):
-    return render(request, 'board/view.html')
+def list(request):
+    bdlist = Board.objects.values('id', 'title', 'userid', 'regdate', 'views', 'thumbup')
+    bdlist = bdlist.order_by('-regdate')
+    bds = {'bds':bdlist}
+    return render(request, 'board/list.html', bds)
+
+def view(request, bid):
+    board = Board.objects.get(id=bid)
+    board.views = board.views + 1
+    board.save()
+
+    bd = {'bd': board}
+    return render(request, 'board/view.html', bd)
+
 
 def write(request):
-    return render(request, 'board/write.html')
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/board/list')
+    else:
+        form = BoardForm()
+    return render(request, 'board/write.html', {'form':form})
 
 def update(request):
     return render(request, 'board/update.html')
 
-def delete(request):
-    return render(request, 'board/delete.html')
+def delete(request, bid):
+    bd = Board.objects.get(id=bid)
+    bd.delete()
+
+    return redirect('/board/list') #page 전환하는 명령어 redirect
